e26aa70bdcd207cd08a876212a0b09391c79471f
LABK9 <alunok9@acad.unifor.br>
2023-10-09 10:55:07 -0300

    SEXO

--- CollisionShape2D.gd
+++ CollisionShape2D.gd
@@ -1,4 +1,10 @@
 extends CollisionShape2D
 
 func _ready():
-	pass
+	Serial.connect("direita",self, "_on_direita")
+	Serial.connect("esquerda",self, "_on_esquerda")
+func _on_direita():
+	position.x = position.x+5
+	
+func _on_esquerda():
+	position.x = position.x-5
